2119f5c4e42c420c186f06c6d64a40ffef4b3d9a
rossedfort <rossedfort@gmail.com>
2015-10-28 21:35:22 -0600

    paperclip gem functionality

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,10 @@
 class Article < ActiveRecord::Base
-  has_many :comments
-  has_many :taggings
+  has_many :comments, dependent: :destroy
+  has_many :taggings, dependent: :destroy
   has_many :tags, through: :taggings
+
+  has_attached_file :image
+  validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
 
   validates_presence_of :title,
                         :body
